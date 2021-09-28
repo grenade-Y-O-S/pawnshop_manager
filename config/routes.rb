9dfe022c913bb3df430ddb55364344a13456f868
grenade-Y-O-S <crmson54@gmail.com>
2021-09-28 13:08:05 +0900

    トップビューファイル作成&ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'customers#index'
+
+  Rails.application.routes.draw do
+    resources :customers, only: [:index]
+  end
+
 end
---
+++ test/controllers/pawns_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PawnsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
