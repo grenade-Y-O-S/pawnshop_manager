4bdf9ca83848b3ad06f122e36eda7bf1ccdf6183
grenade-Y-O-S <crmson54@gmail.com>
2021-09-29 20:21:13 +0900

    コントローラー&モデル作成(客データ登録)&ビュー編集

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,5 +7,20 @@ class CustomersController < ApplicationController
   end
 
   def create
+    binding.pry
+    @customer = Customer.new(customer_params)
+    binding.pry
+    if @customer.save
+      redirect_to root_path
+    else
+      render new_customer_path
+    end
   end
+end
+
+private
+
+def customer_params
+  params.require(:customer).permit(:first_name, :last_name, :first_name_kana,
+                                   :last_name_kana, :address, :phone_number, :birth_date, :job)
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,15 @@
 class Customer < ApplicationRecord
+  has_many :pawns
+  has_many :purchases
+
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :address
+    validates :phone_number
+    validates :birth_date
+    validates :job
+  end
 end
